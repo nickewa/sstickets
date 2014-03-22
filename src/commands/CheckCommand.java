5a5b0158c462d6cda9f0f6859ca443eb45aa01ca
dazza <dazza@Linux-Desktop>
2014-03-23 09:59:03 +1100

    test1

--- src/commands/CheckCommand.java
+++ src/commands/CheckCommand.java
@@ -34,7 +34,7 @@ public class CheckCommand extends SubCommandExecutor{
     		}
 		
 	}
-	//sdtsdfsdf
+	//test
 	@command
 	public void Null (CommandSender sender, String[] args) {
 	    	String[] page1 = Utils.addInFront(args, "1");
